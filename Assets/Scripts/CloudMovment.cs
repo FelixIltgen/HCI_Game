4b33d0b4f8f93f437438f0da7ca5b26b61f8b2fc
FelixIltgen <felixiltgen@hotmail.de>
2024-05-03 15:55:09 +0200

    Added cloud prtotype + initial cloud movement

--- Assets/Scripts/CloudMovment.cs
+++ Assets/Scripts/CloudMovment.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class CloudMovment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        print("Hakke");
-    }
-
+   public float speed;
     // Update is called once per frame
     void Update()
     {
-        
+       transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
     }
 }
