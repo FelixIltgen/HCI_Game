c3718288079413a717d11d7ec028c0c35badc3a7
FelixIltgen <felixiltgen@hotmail.de>
2024-05-04 11:47:34 +0200

    Audio detection (Build started)

--- Assets/Scripts/CloudMovment.cs
+++ Assets/Scripts/CloudMovment.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class CloudMovment : MonoBehaviour
 {
    public float speed;
+   public AudioSource source;
+   public Vector3 minTranslate;
+   public Vector3 maxTranslate;
+   public AudioDetection detector;
+   public float sensibillity = 100;
+   public float threshold = 0.1f;
     // Update is called once per frame
     void Update()
     {
