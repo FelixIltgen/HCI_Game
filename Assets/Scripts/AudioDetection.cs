a94fcceb5a396c66682223d9285c490e6c9ab0f0
FelixIltgen <felixiltgen@hotmail.de>
2024-05-05 17:06:28 +0200

    Removed current audio detection

--- Assets/Scripts/AudioDetection.cs
+++ Assets/Scripts/AudioDetection.cs
@@ -4,49 +4,15 @@ using UnityEngine;
 
 public class AudioDetection : MonoBehaviour
 {
-    public int sampleWindow = 64;
-    private AudioClip microphoneClip;
     // Start is called before the first frame update
     void Start()
     {
-        microphoneToAudioClip();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public void microphoneToAudioClip(){
-
-        string microphoneName = Microphone.devices[0];
-        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
-    }
-
-    public float getAduioFromMicrophone(){
-        return getAudio(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
-    }
-
-    public float getAudio(int position, AudioClip clip){
-
-        int startPosition = position - sampleWindow;
-
-        if (startPosition < 0)
-        {
-            return 0;
-        }
-
-        float[] waveData = new float[sampleWindow];
-        clip.GetData(waveData, startPosition);
-
-        //get loudness
-        float totalLoudness = 0;
-
-        for (int i = 0; i < sampleWindow; i++)
-        {
-            totalLoudness += Mathf.Abs(waveData[i]);
-        }
-
-        return totalLoudness /sampleWindow;
     }
 }
--- Assets/Scripts/CloudMovment.cs
+++ Assets/Scripts/CloudMovment.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class CloudMovment : MonoBehaviour
 {
    public float speed;
-   public AudioSource source;
-   public Vector3 minTranslate;
-   public Vector3 maxTranslate;
-   public AudioDetection detector;
-   public float sensibillity = 100;
-   public float threshold = 0.1f;
+ 
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+       //transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+
+      
     }
 }
