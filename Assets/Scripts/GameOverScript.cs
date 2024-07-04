31c1943a071740eaa2e043608e2710df15a5d170
FelixIltgen <felixiltgen@hotmail.de>
2024-07-04 15:05:37 +0200

    fixed game over button

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -18,6 +18,6 @@ public class GameOverScript : MonoBehaviour
     }
     
     public void MainMenuButton(){
-        //SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu");
     }
 }
